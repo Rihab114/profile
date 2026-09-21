e7f7ec2812b8c60718fde4777c78b24b2c11a598
Gitlab Runner <rihab.ghrab@infor.com>
2026-09-21 12:39:28 +0100

    fix: update site

--- src/data/config.ts
+++ src/data/config.ts
@@ -3,7 +3,7 @@ export const CONFIG = {
   // Site Settings
   // ---------------------------------------------------------------------------
   site: {
-    url: "https://rihab114.github.io/profile/",
+    url: "https://wecarecot.me/profile/",
     locale: "en_US",
     twitterHandle: "",
   },
